72980914b9e0c763af2cbc0b46350407482da287
Pedro Ollero <pedroollero1998@gmail.com>
2025-07-31 17:51:28 +0200

    Update styles

--- frontend/src/components/ModifyButton.tsx
+++ frontend/src/components/ModifyButton.tsx
@@ -2,7 +2,7 @@ const ModifyButton = ({ editRef }: { editRef: string }) => {
   return (
     <button
       onClick={() => (window.location.href = editRef)}
-      className="bg-accent-400 text-white py-4 px-8 text-2xl mt-4 lg:mt-8"
+      className="bg-accent-400 hover:bg-white text-white hover:text-black hover:shadow-md py-4 px-8 text-2xl mt-4 lg:mt-8 rounded-md cursor-pointer"
     >
       Modificar
     </button>
